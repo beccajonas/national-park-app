e2114fc7a38af56bd3c72239079c6d47113ab42b
Becca Jonas <beccajonas3@gmail.com>
2024-01-22 09:40:34 -0500

    add post request for posts

--- server/app.py
+++ server/app.py
@@ -19,16 +19,35 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-
+# Get requests
 @app.get("/")
 def index():
     return "national parks backend"
+# Post posts
+@app.post('/posts')
+def post_photo():
+    print('reached')
+    try:
+        data = request.json
+
+        new_post = Post(
+            caption=data.get("caption"),
+            user_id=data.get("user_id"),
+            park_id=data.get("park_id")
+        )
+
+        db.session.add(new_post)
+        db.session.commit()
+        
+        return new_post.to_dict(), 201
+
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 400 
 
 @app.get("/users")
 def get_users():
     users = User.query.all()
     return [u.to_dict(rules=['-password', '-posts.comments', '-posts.description', '-posts.id', '-posts.name']) for u in users]
-
 
 @app.get("/comments")
 def get_comments():
@@ -38,12 +57,25 @@ def get_comments():
 @app.get("/posts")
 def get_posts():
     posts = Post.query.all()
-    return [p.to_dict() for p in posts]
+    return [p.to_dict(rules=['-park', '-user']) for p in posts]
 
 @app.get("/parks")
 def get_parks():
     parks = Park.query.all()
     return [p.to_dict(rules=['-posts.comments']) for p in parks]
 
+# Get by ID
+@app.get("/users/<int:id>")
+def get_users_by_id(id):
+    user = db.session.get(User, id)
+    return user.to_dict(rules=['-password'])
+
+@app.get("/parks/<int:id>")
+def get_parks_by_id(id):
+    park = db.session.get(Park, id)
+    return park.to_dict()
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
--- server/models.py
+++ server/models.py
@@ -33,7 +33,7 @@ follower_relationship = db.Table(
 class User(db.Model, SerializerMixin):
     __tablename__ = 'user_table'
 
-    serialize_rules = ['-comments', '-followers.posts', '-followers.followers','-followers.password', 'posts.comments']
+    serialize_rules = ['-comments', '-followers.posts', '-followers.followers','-followers.password', '-posts.comments', '-posts.user']
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
@@ -54,7 +54,7 @@ class User(db.Model, SerializerMixin):
 class Post(db.Model, SerializerMixin):
     __tablename__ = 'post_table'
 
-    serialize_rules=['-user.posts','-park.posts', '-comments.post', 'comments.user']
+    serialize_rules=['-user.posts','-park.posts', '-comments.post', '-comments.user']
 
     id = db.Column(db.Integer, primary_key=True)
     caption = db.Column(db.String(255), nullable=False)
