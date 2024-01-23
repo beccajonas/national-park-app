90e47127b4aec6c2a2588ccaead7d23fbc897d08
Becca Jonas <beccajonas3@gmail.com>
2024-01-23 10:12:32 -0500

    add delete requests

--- server/app.py
+++ server/app.py
@@ -63,6 +63,7 @@ def logout():
     session.pop('user_id')
     return { "message": "Logged out"}, 200
 
+# Post photo + post
 @app.post('/upload')
 def upload_photo():
     if 'file' not in request.files:
@@ -100,7 +101,6 @@ def upload_photo():
         return "Credentials not available"
 
     
-
 # Post user
 @app.post('/users')
 def post_new_user():
@@ -126,27 +126,6 @@ def post_new_user():
     except Exception as e:
         return {"Error": str(e)}, 400
 
-# Post posts
-@app.post('/posts')
-def post_photo():
-
-    try:
-        data = request.json
-
-        new_post = Post(
-            caption=data.get("caption"),
-            user_id=data.get("user_id"),
-            park_id=data.get("park_id")
-        )
-
-        db.session.add(new_post)
-        db.session.commit()
-        
-        return new_post.to_dict(), 201
-
-    except Exception as e:
-        return {"Error": str(e)}, 400 
-
 # Post comments
 @app.post('/comments')
 def post_comments():
@@ -167,7 +146,7 @@ def post_comments():
     except Exception as e:
         return {"Error": str(e)}, 400 
 
-# Get requests
+# Get users
 @app.get("/users")
 def get_users():
     users = User.query.all()
@@ -183,17 +162,19 @@ def get_users():
         '-posts.park', 
         '-posts.user_id']) for u in users]
 
+# Get posts
 @app.get("/posts")
 def get_posts():
     posts = Post.query.all()
     return [p.to_dict(rules=['-park', '-user']) for p in posts]
 
+# Get parks
 @app.get("/parks")
 def get_parks():
     parks = Park.query.all()
     return [p.to_dict(rules=['-posts.comments', '-posts.user']) for p in parks]
 
-# Get by ID
+# Get user by ID
 @app.get("/users/<int:id>")
 def get_users_by_id(id):
     user = db.session.get(User, id)
@@ -204,17 +185,19 @@ def get_users_by_id(id):
         '-followers.username',
         '-posts.park'])
 
+# Get park by ID
 @app.get("/parks/<int:id>")
 def get_parks_by_id(id):
     park = db.session.get(Park, id)
     return park.to_dict(rules=['-posts.user', '-posts.comments'])
 
+# Get posts by ID
 @app.get("/posts/<int:id>")
 def get_posts_by_id(id):
     post = db.session.get(Post, id)
     return post.to_dict(rules=['-park', '-user'])
 
-# Patch requests
+# Patch a post
 @app.patch("/posts/<int:id>")
 def patch_post_likes(id):
     data = request.json
@@ -227,6 +210,26 @@ def patch_post_likes(id):
     db.session.commit()
 
     return post.to_dict(rules=['-user']), 201 
+
+# Delete post
+@app.delete("/posts/<int:id>")
+def delete_post_by_id(id):
+    post = db.session.get(Post, id)
+    if not post:
+        return {"error": "post not found"}, 404
+    db.session.delete(post)
+    db.session.commit()
+    return {}
+
+# Delete comment
+@app.delete("/comments/<int:id>")
+def delete_comment_by_id(id):
+    comment = db.session.get(Comment, id)
+    if not comment:
+        {"error": "comment not found"}, 404
+    db.session.delete(comment)
+    db.session.commit()
+    return {}
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
