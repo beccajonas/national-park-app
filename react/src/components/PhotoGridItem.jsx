fcaae9c63b82d5738c634bbdabf616fbbe1afdce
Becca Jonas <beccajonas3@gmail.com>
2024-01-25 12:23:47 -0500

    update models to require values for posts

--- react/src/components/PhotoGridItem.jsx
+++ react/src/components/PhotoGridItem.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import {useState} from "react";
 import "../App.css";
 
 
 function PhotoGridItem({ post, handlePhotoClick }) {
+  const [parkName, setParkName] = useState("")
 
   function handleDetailClick() {
     handlePhotoClick(post)
-      console.log(post);
   }
 
-
+  fetch(`http://localhost:5555/parks/${post.park_id}`)
+  .then(res => res.json())
+  .then(data => setParkName(data.name))
 
   return (
     <div className="bg-gray-200 p-4 rounded h-80 w-80">
       <h2 className="text-lg font-semibold">{post.title}</h2>
       <img onClick={handleDetailClick} src={post.photo_url} alt={post.title} className="mt-2 rounded object-cover w-60 h-60" />
       <button>♥{post.likes}</button>
+      <></>
+      <a href={`http://localhost:5173/parks/${post.park_id}`}>{parkName}</a>
     </div>
   );
 };
